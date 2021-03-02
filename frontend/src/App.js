546c3a4f3bbf8d052f0f36fc3f2ac2a3e595f4d0
gjeffgolden <gjgolden08@gmail.com>
2021-03-02 11:38:54 -0700

    Backend connected to Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,19 @@ import './App.css';
 import React, {Component} from 'react'
 
 class App extends Component {
+
+  state = {
+    jokes: []
+  }
+
+  componentDidMount = () => {
+    fetch('http://localhost:3000/jokes')
+      .then(response => response.json())
+      .then(jokes => this.setState({
+        jokes
+      }))
+  }
+
   render() {
     return (
       <div className="App">
